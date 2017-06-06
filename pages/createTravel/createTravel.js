6d802cb3ec2e22f3d00fcd2a33b38f6e4a911a18
limt <921136839@qq.com>
2017-06-06 09:45:25 +0800

    add iconfont init pages

---
+++ pages/createTravel/createTravel.js
@@ -0,0 +1,22 @@
+// pages/travelManagement/travelManagement.js
+Page({
+    data: {},
+    onLoad: function (options) {
+        wx.setNavigationBarTitle({
+            title: '创建游记'
+        })
+        // 页面初始化 options为页面跳转所带来的参数
+    },
+    onReady: function () {
+        // 页面渲染完成
+    },
+    onShow: function () {
+        // 页面显示
+    },
+    onHide: function () {
+        // 页面隐藏
+    },
+    onUnload: function () {
+        // 页面关闭
+    }
+})
--- pages/index/index.js
+++ pages/index/index.js
@@ -3,23 +3,27 @@
 var app = getApp()
 Page({
   data: {
-    motto: 'Hello World',
     userInfo: {}
   },
   //事件处理函数
-  bindViewTap: function() {
+  CreateTravel: function () {
     wx.navigateTo({
-      url: '../logs/logs'
+      url: '../createTravel/createTravel'
+    })
+  },
+  TravelManagement: function () {
+    wx.navigateTo({
+      url: '../travelManagement/travelManagement'
     })
   },
   onLoad: function () {
     console.log('onLoad')
     var that = this
     //调用应用实例的方法获取全局数据
-    app.getUserInfo(function(userInfo){
+    app.getUserInfo(function (userInfo) {
       //更新数据
       that.setData({
-        userInfo:userInfo
+        userInfo: userInfo
       })
     })
   }
