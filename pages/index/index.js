48c865caaa81b2c8cd556f2dde138bff8335c0be
limt <921136839@qq.com>
2017-07-06 11:36:46 +0800

    Signed-off-by: limt <921136839@qq.com>

--- pages/index/index.js
+++ pages/index/index.js
@@ -21,9 +21,26 @@ Page({
     //调用应用实例的方法获取全局数据
     app.getUserInfo(function (userInfo) {
       //更新数据
+      console.log(userInfo)
       that.setData({
         userInfo: userInfo
       })
+      wx.login({
+        success: function (res) {
+          if (res.code) {
+            console.log(res)
+            //发起网络请求
+            wx.request({
+              url: 'https://www.mingomin.com/service/public/server/getOpenid',
+              data: {
+                code: res.code
+              }
+            })
+          } else {
+            console.log('获取用户登录态失败！' + res.errMsg)
+          }
+        }
+      });
     })
   }
 })
